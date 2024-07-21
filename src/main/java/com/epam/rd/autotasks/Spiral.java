0778b51a06fb83a4368eec3f2e17e959d55f4325
Rahul D <rahul_d@epam.com>
2024-07-21 10:49:00 +0530

    My third Commit

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -4,7 +4,7 @@ class Spiral {
     static int[][] spiral(int rows, int columns) {
 
         int up,down,left,right;
-        up = -1;
+        up = 0;
         down = rows;
         left = -1;
         right = columns;
@@ -59,4 +59,17 @@ class Spiral {
 
         return arr;
     }
+
+//    public static void main(String[] args) {
+//        int [][] ans = Spiral.spiral(3,4);
+//
+//        for(int i=0;i<ans.length;i++)
+//        {
+//            for(int j=0;j<ans[0].length;j++)
+//            {
+//                System.out.print(ans[i][j] +" ");
+//            }
+//            System.out.println();
+//        }
+//    }
 }
